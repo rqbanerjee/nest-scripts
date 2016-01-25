4de9434b4704430463e04aa5346dccf0bcc2007b
rqbanerjee <rqbanerjee@gmail.com>
2016-01-24 22:48:18 -0500

    tweak error if mysql2 is not installed

--- init_nest.rb
+++ init_nest.rb
@@ -25,9 +25,10 @@ end
 begin
     require_relative 'lib/db_writer'
     db_writer = DbWriter.new(nest_config)
-rescue Gem::LoadError => e
-    puts "****\n Not going attempting to connect to mysql since mysql2 gem is not installed \n****"
+rescue Exception, Gem::LoadError => e
+    puts "****\n Not going attempting to connect to mysql since mysql2 gem is not installed. Quitting \n****"
 	puts e
+    exit -1
 end
 
 db_writer.store_readings(thermostats)
